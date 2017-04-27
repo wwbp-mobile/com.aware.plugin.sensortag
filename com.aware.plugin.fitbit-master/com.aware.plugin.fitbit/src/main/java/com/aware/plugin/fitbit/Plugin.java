9ed6a7ad2a3f71bb6a8beeb3a0baf59cc4f81be6
achadha0111 <achadharocks@gmail.com>
2017-04-27 12:24:15 +0100

    Added working device scanner
    TODO
    1) Implement selection functionality

---
+++ com.aware.plugin.fitbit-master/com.aware.plugin.fitbit/src/main/java/com/aware/plugin/fitbit/Plugin.java
@@ -0,0 +1,97 @@
+package com.aware.plugin.fitbit;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import com.aware.Aware;
+import com.aware.Aware_Preferences;
+import com.aware.utils.Aware_Plugin;
+
+public class Plugin extends Aware_Plugin {
+
+    public static BLEDevicePicker bleDevicePicker = null;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        TAG = "AWARE::"+getResources().getString(R.string.app_name);
+
+        /**
+         * Plugins share their current status, i.e., context using this method.
+         * This method is called automatically when triggering
+         * {@link Aware#ACTION_AWARE_CURRENT_CONTEXT}
+         **/
+        CONTEXT_PRODUCER = new ContextProducer() {
+            @Override
+            public void onContext() {
+                //Broadcast your context here
+            }
+        };
+
+        //Add permissions you need (Android M+).
+        //By default, AWARE asks access to the #Manifest.permission.WRITE_EXTERNAL_STORAGE
+
+        //REQUIRED_PERMISSIONS.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        //To sync data to the server, you'll need to set this variables from your ContentProvider
+        DATABASE_TABLES = Provider.DATABASE_TABLES;
+        TABLES_FIELDS = Provider.TABLES_FIELDS;
+        CONTEXT_URIS = new Uri[]{ Provider.TableOne_Data.CONTENT_URI }; //this syncs dummy TableOne_Data to server
+    }
+
+    //This function gets called every 5 minutes by AWARE to make sure this plugin is still running.
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+
+        bleDevicePicker = new BLEDevicePicker();
+        bleDevicePicker.execute();
+
+        if (PERMISSIONS_OK) {
+
+            DEBUG = Aware.getSetting(this, Aware_Preferences.DEBUG_FLAG).equals("true");
+
+            //Initialize our plugin's settings
+            Aware.setSetting(this, Settings.STATUS_PLUGIN_TEMPLATE, true);
+
+            //Initialise AWARE instance in plugin
+            Aware.startAWARE(this);
+        }
+
+        return START_STICKY;
+    }
+
+    public class BLEDevicePicker extends AsyncTask<Void, Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+
+            Intent devicePicker = new Intent(getApplicationContext(), DevicePicker.class);
+            devicePicker.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(devicePicker);
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            super.onPostExecute(result);
+            if (!result) {
+                Toast.makeText(getApplicationContext(), "Failed to find devices", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        Aware.setSetting(this, Settings.STATUS_PLUGIN_TEMPLATE, false);
+
+        //Stop AWARE instance in plugin
+        Aware.stopAWARE(this);
+    }
+}
